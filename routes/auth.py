f7c3107b28a00a4af59af23dd64af20f239d7afc
liuyunsong3 <liuyunsong3@xiaomi.com>
2025-05-26 16:49:29 +0800

    login增加csrf验证

--- routes/auth.py
+++ routes/auth.py
@@ -2,21 +2,21 @@ from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required
 from . import auth_bp
 from models import User
+from forms.auth import LoginForm
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        user = User.query.filter_by(username=username).first()
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
         
-        if user and user.check_password(password):
+        if user and user.check_password(form.password.data):
             login_user(user)
             return redirect(url_for('admin.dashboard'))
         
         flash('用户名或密码错误')
     
-    return render_template('admin/login.html')
+    return render_template('admin/login.html', form=form)
 
 @auth_bp.route('/logout')
 @login_required

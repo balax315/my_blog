255e62f9ba47f3f01e83d9f6ba5c73bf597c1261
liuyunsong3 <liuyunsong3@xiaomi.com>
2025-06-03 20:21:03 +0800

    更新依赖

--- forms/post.py
+++ forms/post.py
@@ -5,7 +5,6 @@ from wtforms.validators import DataRequired, Length
 
 class PostForm(FlaskForm):
     title = StringField('标题', validators=[DataRequired(), Length(max=200)])
-    title2 = StringField('标题2', validators=[DataRequired(), Length(max=200)])
     slug = StringField('URL别名', validators=[DataRequired(), Length(max=200)])
     category_id = SelectField('分类', coerce=int)
     content = TextAreaField('内容')  # 不加 validators=[DataRequired()]
